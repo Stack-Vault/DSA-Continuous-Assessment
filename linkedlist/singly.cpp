d215e4f927af949cc0891cf4d13260a1d9bf1f95
Amani Ishimwe <amaniishimwe36@gmail.com>
2026-02-19 19:53:01 +0200

    add proper indentation

--- linkedlist/singly.cpp
+++ linkedlist/singly.cpp
@@ -1,88 +1,118 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node {
+public:
     int data;
     Node* next;
 };
- 
-//  printing the linked list
-void print_list(Node* head){
+
+// ---------------------------------------------
+// Print the linked list
+// ---------------------------------------------
+void print_list(Node* head) {
     Node* temp = head;
-    while(temp != NULL){
-        cout << temp->data << " "<<endl;
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
         temp = temp->next;
     }
+
+    cout << endl;
 }
 
- // counting elements of linked list
-  int count_nodes(Node* head){
-	int count = 0 ;
-	Node* ptr = head;
-	
-	while(ptr != NULL){
-		count++;
-		ptr = ptr->next;
-	}
+// ---------------------------------------------
+// Count nodes (Iterative)
+// ---------------------------------------------
+int count_nodes(Node* head) {
+    int count = 0;
+    Node* ptr = head;
+
+    while (ptr != NULL) {
+        count++;
+        ptr = ptr->next;
+    }
+
     return count;
 }
-/*
 
-counting nodes using recursive funciton
-int count_nodes_recursive(Node* head){
-	int count;
-	if(head == NULL){
-		count =0;
-		return count;
-	}else{
-		count = 1;
-		return count + count_nodes_recursive(head->next);
-	}
+/*
+// ---------------------------------------------
+// Count nodes (Recursive)
+// ---------------------------------------------
+int count_nodes_recursive(Node* head) {
+    if (head == NULL) {
+        return 0;
+    }
+    return 1 + count_nodes_recursive(head->next);
 }
 */
 
-void insert_node_beginning(Node*& head, int value){
-	Node* newNode = new Node();
-	newNode->data = value;
-	newNode->next = head;
-	head = newNode;
-}
-void insert_node_end(Node*& head, int value){
-	Node* newNode = new Node();
-	newNode->data = value;
-	newNode->next = NULL;
-	
-	if (head == NULL){
-		head = newNode;
-		return; // used to end programm running 
-	}
-	
-	//traverse throuht the list
-	Node* temp = head;
-	while(temp->next != NULL){
-		temp = temp->next;
-	}
-		
-	temp->next = newNode;
+// ---------------------------------------------
+// Insert node at beginning
+// ---------------------------------------------
+void insert_node_beginning(Node*& head, int value) {
+    Node* newNode = new Node();
+
+    newNode->data = value;
+    newNode->next = head;
+    head = newNode;
 }
 
-int main(){
+// ---------------------------------------------
+// Insert node at end
+// ---------------------------------------------
+void insert_node_end(Node*& head, int value) {
+    Node* newNode = new Node();
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    // If list is empty
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    // Traverse to the last node
+    Node* temp = head;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
+
+// ---------------------------------------------
+// Main Function
+// ---------------------------------------------
+int main() {
     Node* head = NULL;
+
     Node n1, n2, n3;
 
     n1.data = 10;
     n1.next = &n2;
+
     n2.data = 20;
     n2.next = &n3;
-    n3.data = 30;   
+
+    n3.data = 30;
     n3.next = NULL;
 
     head = &n1;
-    
+
+    // Insert operations
     insert_node_beginning(head, 5);
     insert_node_end(head, 40);
+
+    // Print list
     print_list(head);
+
+    // Count nodes
     int total = count_nodes(head);
-    cout << "The count is "<< total << endl;
+    cout << "The count is " << total << endl;
+
+    return 0;
 }
